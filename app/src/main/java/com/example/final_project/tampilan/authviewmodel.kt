4de409b395380407ffa34479500682de028941f6
adrianrezha29 <adrianrezha29@gmail.com>
2024-01-11 15:19:08 +0700

    add method signin with email and password on authviewmodel class

--- app/src/main/java/com/example/final_project/tampilan/authviewmodel.kt
+++ app/src/main/java/com/example/final_project/tampilan/authviewmodel.kt
@@ -2,7 +2,7 @@ package com.example.final_project.tampilan
 
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.firebase.auth.FirebaseUser
 
 class authviewmodel : ViewModel() {
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
@@ -11,10 +11,26 @@ class authviewmodel : ViewModel() {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    // Otentikasi sukses, Anda dapat menangani logika sesuai kebutuhan
                 } else {
+                    // Otentikasi gagal, tampilkan pesan kesalahan jika perlu
                 }
             }
     }
 
+    fun signInWithEmailAndPassword(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Otentikasi sukses, Anda dapat menangani logika sesuai kebutuhan
+                } else {
+                    // Otentikasi gagal, tampilkan pesan kesalahan jika perlu
+                }
+            }
+    }
 
+    // Metode lainnya sesuai kebutuhan, seperti pengiriman email verifikasi, cek status otentikasi, dll.
+
+    // Misalnya, untuk mendapatkan pengguna saat ini:
+    fun getCurrentUser(): FirebaseUser? = auth.currentUser
 }

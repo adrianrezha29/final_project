a3291a980c0df46ff8b2ed97f3981457eb3385b4
adrianrezha29 <adrianrezha29@gmail.com>
2024-01-11 17:20:10 +0700

    buat add viewmodel

--- app/src/main/java/com/example/final_project/ui/add/AddViewModel.kt
+++ app/src/main/java/com/example/final_project/ui/add/AddViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.final_project.ui.add
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.final_project.data.LaptopRepository
+import com.example.final_project.ui.AddEvent
+import com.example.final_project.ui.AddUIState
+import com.example.final_project.ui.toLaptop
+
+class AddViewModel(private val laptopRepository: LaptopRepository) : ViewModel() {
+
+    var addUIState by mutableStateOf(AddUIState())
+        private set
+
+    fun updateAddUIState(addEvent: AddEvent) {
+        addUIState = AddUIState(addEvent = addEvent)
+    }
+
+    suspend fun addLaptop() {
+        laptopRepository.save(addUIState.addEvent.toLaptop())
+    }
+}

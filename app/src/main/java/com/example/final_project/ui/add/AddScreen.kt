4dcb229effa81e186df979abe47633d05cb2790f
adrianrezha29 <adrianrezha29@gmail.com>
2024-01-11 17:43:05 +0700

    add Screen UI

--- app/src/main/java/com/example/final_project/ui/add/AddScreen.kt
+++ app/src/main/java/com/example/final_project/ui/add/AddScreen.kt
@@ -1,7 +1,33 @@
 package com.example.final_project.ui.add
 
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.final_project.navigation.DestinasiNavigasi
+import com.example.final_project.ui.AddEvent
+import com.example.final_project.ui.AddUIState
+import com.example.final_project.ui.LaptopTopAppBar
+import com.example.final_project.ui.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 
 object DestinasiEntry : DestinasiNavigasi {
@@ -9,3 +35,109 @@ object DestinasiEntry : DestinasiNavigasi {
     override val titleRes = "Entry Laptop"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    addViewModel: AddViewModel = viewModel(factory = PenyediaViewModel.Factory),
+
+    ) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            LaptopTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+
+        EntryBody(
+            addUIState = addViewModel.addUIState,
+            onSiswaValueChange = addViewModel::updateAddUIState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    addViewModel.addLaptop()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryBody(
+    addUIState: AddUIState,
+    onSiswaValueChange: (AddEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            addEvent = addUIState.addEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Submit")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    addEvent: AddEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (AddEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = addEvent.nama,
+            onValueChange = { onValueChange(addEvent.copy(nama = it)) },
+            label = { Text("Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = addEvent.jenis,
+            onValueChange = { onValueChange(addEvent.copy(jenis = it)) },
+            label = { Text("Jenis") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = addEvent.harga,
+            onValueChange = { onValueChange(addEvent.copy(harga = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(text = "Harga") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+    }
+}

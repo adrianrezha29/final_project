b72043d3d15e8e3c9b709f70337515053639a3d2
adrianrezha29 <adrianrezha29@gmail.com>
2024-01-11 17:22:13 +0700

    buat object DestinasiEntry

---
+++ app/src/main/java/com/example/final_project/ui/add/AddScreen.kt
@@ -0,0 +1,11 @@
+package com.example.final_project.ui.add
+
+
+import com.example.final_project.navigation.DestinasiNavigasi
+
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Laptop"
+}
+
